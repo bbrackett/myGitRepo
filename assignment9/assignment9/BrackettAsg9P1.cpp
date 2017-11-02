abb7877dbc5673fb221544b682e3176edf8848a1
Brent Brackett <bebrackett@outlook.com>
2017-11-02 09:49:29 -0500

    Working assignments

---
+++ assignment9/assignment9/BrackettAsg9P1.cpp
@@ -0,0 +1,18 @@
+/****************************************************************************
+
+
+
+****************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+// Function Prototypes
+double getSales(string);
+void findHighest(double, double, double, double);
+
+int main()
+{
+
+}
